094f37655bf0fc65a1e1f376fa8c9cd1ff8a722d
mauricio <mauriciogtec@gmail.com>
2016-10-05 19:03:03 -0500

    no tengo tiempo para esto

---
+++ R/markovChains/mc_cpp_functions.cpp
@@ -0,0 +1,36 @@
+#include <Rcpp.h>
+using namespace Rcpp; 
+using namespace std;
+
+// [[Rcpp::export]]
+int sampleC(NumericVector prob) {
+  // RcppArmadillo ya tiene una version de sample
+  NumericVector prob_normalized = prob/sum(prob); // por si las probabilidades no suma 1 como deben
+  NumericVector cumprob = cumsum(prob_normalized);
+  // randon uniform between zero and one
+  double u = runif(1)[0];
+  int i=0; // posiciones al estilo R, primero 1
+  while (cumprob[i] < u) {
+    i++;
+  }
+  return i + 1; // el +1  es porque R cuenta indices distinto
+}
+
+// [[Rcpp::export]]
+int mc_transition(int current_state, NumericMatrix trans_mat) {
+  NumericVector prob =  trans_mat(current_state-1,_); // el -1 es por diferencias indices R y C++
+    return sampleC(prob);
+}
+
+
+// [[Rcpp::export]]
+IntegerVector mc_sim(int init_state, int n_transitions, NumericMatrix trans_mat) {
+  IntegerVector trajectory(n_transitions + 1); // el estado inicial mas el numero de transicion
+  trajectory[0] = init_state;
+  for (int i=0; i < n_transitions; i++) {
+    trajectory[i + 1] = markovchain_transition(trajectory[i], trans_mat);
+  }
+  return trajectory;
+}
+
+
